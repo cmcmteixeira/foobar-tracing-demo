650d6b4e579a7b60e7eb0bd060a1da7e6e47dd5f
cmcmteixeira <cmcmteixeira@gmail.com>
2018-08-28 16:37:54 +0100

    Fixed compose configs + added ability to parallelize request to pour drinks

--- pourdrinks.sh
+++ pourdrinks.sh
@@ -3,9 +3,14 @@
 WATER=0
 SODA=0
 COKE=0
+MAX_PARALLEL=1
 for i in "$@"
 do
 case $i in
+    --par=*)
+    MAX_PARALLEL="${i#*=}"
+    shift # past argument=value
+    ;;
     --water=*)
     WATER="${i#*=}"
     shift # past argument=value
@@ -24,14 +29,17 @@ case $i in
 esac
 done
 
+ACC_STR=''
 for ((i=1; i<= SODA; i++)); do
-    curl -X POST 127.0.0.1:9999/drinkRequest -d '{"drink": "soda"}' -w "\n" 
+    ACC_STR="soda\n$ACC_STR"
 done
 
-for ((i=1; i <=$WATE WATER; i++)); do
-    curl -X POST 127.0.0.1:9999/drinkRequest -d '{"drink": "water"}' -w "\n" 
+for ((i=1; i <=$WATER; i++)); do
+    ACC_STR="water\n$ACC_STR"
 done
 
-for ((i=1; i <= COKE; i++)); do
-    curl -X POST 127.0.0.1:9999/drinkRequest -d '{"drink": "coke"}' -w "\n"  
+for ((i=1; i <=$COKE; i++)); do
+    ACC_STR="coke\n$ACC_STR"
 done
+
+printf $ACC_STR | gshuf - | xargs -P $MAX_PARALLEL -I {} curl -X POST 127.0.0.1:9999/drinkRequest -d '{"drink": "{}"}' -w "\n"
